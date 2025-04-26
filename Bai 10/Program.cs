3014db68d769e7df9d86e34825c539301b751d96
Duy0305 <chinguyetduy2005@gmail.com>
2025-04-26 13:56:42 +0700

    Add files via upload

---
+++ Bai 10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Điểm quá trình : ");
+            double diemQT = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Điểm thực hành  : ");
+            double diemTH = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Điểm thi cuối kỳ  : ");
+            double diemCK = double.Parse(Console.ReadLine());
+
+            double diemTrungBinh = (diemQT * 0.2) + (diemCK * 0.5) + (diemTH * 0.3);
+
+            diemTrungBinh = Math.Round(diemTrungBinh, 1);
+
+            Console.WriteLine($"Điểm trung bình môn học: {diemTrungBinh}");
+            Console.ReadKey();
+
+        }
+    }
+}
